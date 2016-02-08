4b4daee2e3016fc99c09fbc0e412e64e945aeafa
unknown <admin@A2-4.itstep.lan>
2016-02-08 21:11:40 +0300

    dz

---
+++ ADO.net/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    //class MyEventArgs : EventArgs
+    //{
+    //    public string ch;
+    //}
+    //public interface ISay
+    //{
+    //    void Say();
+    //}
+    //class Saymen: ISay
+    //{
+    //    public void Say()
+    //    {
+    //        Console.WriteLine("Spich");
+    //    }
+    //}
+    //class Saybull: ISay
+    //{
+    //    public void Say()
+    //    {
+    //        Console.WriteLine("Mu");
+    //    }
+    //}
+    //class Bull
+    //{
+    //    public event EventHandler<MyEventArgs> Event;
+    //    public void an_object_which_sees_the_minotaur(string ch)
+    //    {
+    //        MyEventArgs c = new MyEventArgs();
+
+    //            c.ch = ch;
+    //            Event(this, c);
+    //    }
+    //    public ISay s { private get; set; }
+    //    public void SetISay(ISay s)
+    //    {
+    //        this.s = s;
+    //    }
+    //    public Bull(ISay s)
+    //    {
+    //        this.s = s;
+    //    }
+    //    public void Say()
+    //    {
+    //        s.Say();
+    //    }
+    //}
+    //class Man
+    //{ 
+    //     public ISay s { private get; set; }
+    //     public Man(ISay s)
+    //    {
+    //        this.s = s;
+    //    }
+    //    public void Say()
+    //    {
+    //        s.Say();
+    //    }
+    //}
+    static class Class1
+    {
+        static public int h;
+        public Class1()
+        {
+            Console.WriteLine("Default");
+        }
+        public Class1(ref int r)
+        {
+                h = r;
+                Console.WriteLine("Static");
+        }
+    }
+    static class Class2
+    {
+        static public int h;
+        public Class2()
+        {
+            Console.WriteLine("Default");
+        }
+        public Class2(ref int r)
+        {
+                h = r;
+                Console.WriteLine("Static");
+        }
+    }
+    class Program
+    {
+        int b = 10;
+        static void Main(string[] args)
+        {
+            Class1 c1 = new Class1(ref b);
+
+        //    Class1 c1 = new Class1(10);
+        //    Console.WriteLine(c1.h);
+        //    int h = 19;
+        //    Class2 c2 = new Class2(ref h);
+        //    Console.WriteLine(c2.h);
+        ////    Man m = new Man(new Saymen());
+        //    Bull b = new Bull(new Saybull());
+        //    m.Say();
+        //    b.Say();
+        //    b.Event += (sender, e) =>
+        //        {
+        //            switch(e.ch)
+        //            {
+        //                case "cow":
+        //                {
+        //                    b.SetISay(new Saybull());
+        //                    break;
+        //                }
+        //                case "men":
+        //                {
+        //                    b.SetISay(new Saymen());
+        //                    break;
+        //                }
+        //                default: break;   
+        //            }
+        //        };
+        //    b.an_object_which_sees_the_minotaur("men");
+        //    b.Say();
+        //    b.an_object_which_sees_the_minotaur("cow");
+        //    b.Say();
+        }
+    }
+}
---
+++ ADO.net/day_9LINQ_toXML_EXCEL/day_9LINQ_toXML_EXCEL/Program.cs
@@ -0,0 +1,53 @@
+﻿using LinqToExcel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_9LINQ_toXML_EXCEL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Получение информации из Excel документа с именем SampleData.xls
+            string pathFile = "PriceProductBase.xlsx";
+
+            var excelFile = new ExcelQueryFactory(pathFile);
+
+            string sheetName = "Orders";
+
+            // Вывод списка листов (Worksheet) в Excel документе  
+
+            var infoOrders = from a in excelFile.Worksheet(sheetName)
+                             select a;
+
+            foreach (var item in excelFile.GetWorksheetNames())
+            {
+                foreach (var itemRow in excelFile.GetColumnNames(item))
+                {
+                    Console.Write(itemRow + " ");
+                }
+                Console.Write("\n");
+            }
+
+            foreach (var item in infoOrders)
+            {
+                string pattern = "{0};{1};{2};{3};{4};{5};{6};";
+
+                // Вывод информации из столбцов OrderDate и Total
+                Console.WriteLine(string.Format(pattern, item["Номер заказа"],
+                    item["Номер продукта"], item["F3"], 
+                    item["F4"], item["F5"], 
+                    item["F6"], item["F7"]));
+            }
+
+
+            // установка документа "только для чтения"
+            excelFile.ReadOnly = true;
+
+            Console.ReadKey();
+        }
+    }
+}
