4978a2c4ea43c3aaa470a8c05d673efe53a470a4
unknown <admin@A2-4.itstep.lan>
2016-02-15 20:26:08 +0300

    dz

---
+++ ADO.net/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using A = B;
+
+namespace B
+{
+    struct str
+    {
+        public void metod()
+        { 
+        }
+    }
+
+    class Point
+
+{
+
+public int x; public int y;
+
+public override string ToString()
+
+{
+
+return string.Format("X : {0}  Y : {1}\n", x, y);
+}
+    }
+
+        class Program
+        {
+
+            static void Main()
+            {
+                Point p = new Point();
+
+                p.x = 5;
+                p.y = 6;
+
+                fixed (int* ptr = &p.x)
+                {
+
+                    *ptr = 15; Console.WriteLine(p);
+
+                }
+
+
+                Console.WriteLine(p);
+            }
+        }
+}
